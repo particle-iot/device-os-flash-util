e017de839f1280eeaaa832b405fdacd15f8e36d0
Joe Goggins <joe@particle.io>
2021-08-17 16:16:14 -0500

    Have a useful integration test harness

--- integration-tests/module-cache-with-github-token.test.js
+++ integration-tests/module-cache-with-github-token.test.js
@@ -1,22 +1,25 @@
 /**
- * This is a hack to illustrate how to use the ModuleCache object in another project
- * 
- * Usage:
+ * How to run integration test:
  * 
  * 1. Get a personal github access token, with just repo public_repo access
  * 2. Authorize particle-iot SSO GH token 
- * 3. Run this command: `GITHUB_TOKEN=redacted npm test --grep ModuleCache`
+ * 3. Run this command: `GITHUB_TOKEN=redacted npm integration:test`
  */
 
- const chai = require('chai');
- const expect = chai.expect;
- chai.use(require('chai-fs'));
- const { ModuleCache } = require('../lib/module.js');
- const { Logger, LogLevel } = require('../lib/log');
- 
- describe(`ModuleCache can download a pre-determined draft release from GitHub using a GITHUB_TOKEN`, () => {
-     it('can download the mock release 1.9.0-rc.1', async () => {
-         expect(true).to.eql(false);
+const chai = require('chai');
+const expect = chai.expect;
+chai.use(require('chai-fs'));
+const { ModuleCache } = require('../lib/module.js');
+const { Logger, LogLevel } = require('../lib/log');
+
+if (!process.env.GITHUB_TOKEN) {
+    throw new Error("GITHUB_TOKEN env var is not set, this integration test requires it");
+}
+describe(`ModuleCache can download a pre-determined draft release from GitHub using a GITHUB_TOKEN`, () => {
+    it('can download the mock release 1.9.0-rc.1', async () => {
+        // https://github.com/particle-iot/device-os/releases/edit/untagged-0aba6b1c0d9f970be876
+        // boron-tinker@1.9.0-rc.1.bin
+
         //  const deviceOSVersion = '1.9.0-rc.1';
         //  const log = new Logger();
         //  // This doesnt work like you'd expect
@@ -26,14 +29,11 @@
         //      tempDir: 'test-fixtures/module-cache1/tmp',
         //      log
         //  });
- 
-        //  // TODO: move this to integration test
-        //  // if (!process.env.GITHUB_TOKEN) {
-        //  // 	throw new Error("GITHUB_TOKEN env var is not test, mochaCache.init() requires it");
-        //  // }
-         
+
+
+
         //  await moduleCache.init()
- 
+
         //  // Each element in this array looks like this:
         //  //   {
         //  //   	platformId: 25,
@@ -55,6 +55,5 @@
         //      // this file system assertion ability is from https://www.chaijs.com/plugins/chai-fs/
         //      expect(element.file).to.be.a.path();
         //  }
-     });
- });
- 
+    });
+});
