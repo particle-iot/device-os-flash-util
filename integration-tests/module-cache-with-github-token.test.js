99dcb8947fdd18b58683d553c684aa754026408d
Joe Goggins <joe@particle.io>
2021-08-17 16:36:50 -0500

    Introduce place in ModuleCache where code to pull draft release will live

--- integration-tests/module-cache-with-github-token.test.js
+++ integration-tests/module-cache-with-github-token.test.js
@@ -19,41 +19,38 @@ describe(`ModuleCache can download a pre-determined draft release from GitHub us
     it('can download the mock release 1.9.0-rc.1', async () => {
         // https://github.com/particle-iot/device-os/releases/edit/untagged-0aba6b1c0d9f970be876
         // boron-tinker@1.9.0-rc.1.bin
+		const deviceOSVersion = '1.9.0-rc.1';
+		const log = new Logger();
+		// This doesnt work like you'd expect
+		log.level = LogLevel.VERBOSE;
+		const moduleCache = new ModuleCache({
+			cacheDir: 'test-fixtures/module-cache2/cache',
+			tempDir: 'test-fixtures/module-cache2/tmp',
+			log
+		});
+		
+		await moduleCache.init()
 
-        //  const deviceOSVersion = '1.9.0-rc.1';
-        //  const log = new Logger();
-        //  // This doesnt work like you'd expect
-        //  log.level = LogLevel.VERBOSE;
-        //  const moduleCache = new ModuleCache({
-        //      cacheDir: 'test-fixtures/module-cache1/cache',
-        //      tempDir: 'test-fixtures/module-cache1/tmp',
-        //      log
-        //  });
-
-
-
-        //  await moduleCache.init()
-
-        //  // Each element in this array looks like this:
-        //  //   {
-        //  //   	platformId: 25,
-        //  //   	type: 'system_part',
-        //  //   	index: 1,
-        //  //   	version: 2101,
-        //  //   	storage: 'internal_flash',
-        //  //   	address: 196608,
-        //  //   	moduleSize: 496476,
-        //  //   	headerSize: 24,
-        //  //   	dropHeader: false,
-        //  //   	crcValid: true,
-        //  //   	fileSize: 496476,
-        //  //   	file: '/Users/jgoggins/git/particle/device-os-flash-util/test-fixtures/module-cache1/cache/2.1.0/b5som/b5som-system-part1@2.1.0.bin'
-        //  //   },
-        //  const someModules = await moduleCache.getReleaseModules(deviceOSVersion);
-        //  for (const element of someModules) {
-        //      // console.log(`Making assertions on this`, element);
-        //      // this file system assertion ability is from https://www.chaijs.com/plugins/chai-fs/
-        //      expect(element.file).to.be.a.path();
-        //  }
+		// Each element in this array looks like this:
+		//   {
+		//   	platformId: 25,
+		//   	type: 'system_part',
+		//   	index: 1,
+		//   	version: 2101,
+		//   	storage: 'internal_flash',
+		//   	address: 196608,
+		//   	moduleSize: 496476,
+		//   	headerSize: 24,
+		//   	dropHeader: false,
+		//   	crcValid: true,
+		//   	fileSize: 496476,
+		//   	file: '/Users/jgoggins/git/particle/device-os-flash-util/test-fixtures/module-cache1/cache/2.1.0/b5som/b5som-system-part1@2.1.0.bin'
+		//   },
+		const someModules = await moduleCache.getReleaseModules(deviceOSVersion);
+		for (const element of someModules) {
+			// console.log(`Making assertions on this`, element);
+			// this file system assertion ability is from https://www.chaijs.com/plugins/chai-fs/
+			expect(element.file).to.be.a.path();
+		}
     });
 });
