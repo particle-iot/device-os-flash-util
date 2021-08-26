c128a2c764177620a30f79173714745d0def688d
Sergey Polyakov <sergeuz@gmail.com>
2021-08-26 21:11:22 +0200

    Fix tests

--- integration-tests/module-cache-with-github-token.test.js
+++ integration-tests/module-cache-with-github-token.test.js
@@ -1,8 +1,8 @@
 /**
  * How to run integration test:
- * 
+ *
  * 1. Get a personal github access token, with just repo public_repo access
- * 2. Authorize particle-iot SSO GH token 
+ * 2. Authorize particle-iot SSO GH token
  * 3. Run this command: `GITHUB_TOKEN=redacted npm integration:test`
  */
 
@@ -12,8 +12,19 @@ chai.use(require('chai-fs'));
 const { ModuleCache } = require('../lib/module.js');
 const { Logger, LogLevel } = require('../lib/log');
 
+const rimraf = require('rimraf');
+const mkdirp = require('mkdirp');
+const path = require('path');
+
+const TEST_DIR = 'test-fixtures';
+
 describe(`ModuleCache can download a pre-determined draft release from GitHub using a GITHUB_TOKEN`, () => {
-    it('can download a mock 1.9.0-rc.1', async () => {
+	beforeEach(() => {
+		rimraf.sync(TEST_DIR);
+		mkdirp.sync(TEST_DIR);
+	});
+
+	it('can download a mock 1.9.0-rc.1', async () => {
 		if (!process.env.GITHUB_TOKEN) {
 			throw new Error("GITHUB_TOKEN env var is not set, this integration test requires it");
 		}
@@ -21,14 +32,14 @@ describe(`ModuleCache can download a pre-determined draft release from GitHub us
 		const log = new Logger();
 		log.level = LogLevel.VERBOSE;
 		const moduleCache = new ModuleCache({
-			cacheDir: 'test-fixtures/module-cache2/cache',
-			tempDir: 'test-fixtures/module-cache2/tmp',
+			cacheDir: path.join(TEST_DIR, 'cache'),
+			tempDir: path.join(TEST_DIR, 'temp'),
 			log
 		});
 
 		await moduleCache.init()
 
-		const someModules = await moduleCache.getReleaseModules(deviceOSVersion);
+		const someModules = await moduleCache.getReleaseModules(deviceOSVersion, { noDraft: false });
 		for (const element of someModules) {
 			// console.log(`Making assertions on this`, element);
 			// this file system assertion ability is from https://www.chaijs.com/plugins/chai-fs/
@@ -41,8 +52,8 @@ describe(`ModuleCache can download a pre-determined draft release from GitHub us
 		const log = new Logger();
 		log.level = LogLevel.VERBOSE;
 		const moduleCache = new ModuleCache({
-			cacheDir: 'test-fixtures/module-cache2/cache',
-			tempDir: 'test-fixtures/module-cache2/tmp',
+			cacheDir: path.join(TEST_DIR, 'cache'),
+			tempDir: path.join(TEST_DIR, 'temp'),
 			log
 		});
 
@@ -64,8 +75,8 @@ describe(`ModuleCache can download a pre-determined draft release from GitHub us
 		const log = new Logger();
 		log.level = LogLevel.VERBOSE;
 		const moduleCache = new ModuleCache({
-			cacheDir: 'test-fixtures/module-cache2/cache',
-			tempDir: 'test-fixtures/module-cache2/tmp',
+			cacheDir: path.join(TEST_DIR, 'cache'),
+			tempDir: path.join(TEST_DIR, 'temp'),
 			log
 		});
 
